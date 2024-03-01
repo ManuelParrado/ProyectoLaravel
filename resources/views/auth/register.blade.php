046f9b2c7bd040dbec7fbdb31774ff90a7a8e75a
ManuelParrado <manuparradobarrero2004@gmail.com>
2024-03-01 21:00:14 +0100

    registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,7 +10,7 @@
         </div>
 
         <!-- Last Name -->
-        <div>
+        <div class="mt-4">
             <x-input-label for="last_name" :value="__('Last Name')" />
             <x-text-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" required autofocus autocomplete="last_name" />
             <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
@@ -44,14 +44,6 @@
                             name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="relative z-0 w-full mb-5 group mt-4">
-            <input type="file" name="user_image" id="user_image" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" value="{{old('user_image')}}" required />
-            <label for="user_image" class="peer-focus:font-medium absolute text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">User Image</label>
-            @error('user_image')
-                <br><p style="color: red">{{$message}}</p>
-            @enderror
         </div>
 
         <div class="flex items-center justify-end mt-4">
